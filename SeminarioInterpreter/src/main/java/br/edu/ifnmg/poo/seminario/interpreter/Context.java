9d8d8533f55f677af5d08d1bef2dcf376ade9b3d
Dayany Lima Santos <dayanyylima@gmail.com>
2022-06-13 14:09:26 -0300

    Correção de privacidade de variável expressao

--- SeminarioInterpreter/src/main/java/br/edu/ifnmg/poo/seminario/interpreter/Context.java
+++ SeminarioInterpreter/src/main/java/br/edu/ifnmg/poo/seminario/interpreter/Context.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 
 public class Context {
 
-    public String expressao;
+    private String expressao;
     private LocalDateTime data;
 
     public Context(LocalDateTime data) {
